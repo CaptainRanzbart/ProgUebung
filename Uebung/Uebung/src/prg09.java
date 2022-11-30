e398fb83f9988ba254438037ccb86ce6ddfd6451
Colin Schmidt <colin@sei-com.de>
2022-11-30 14:55:18 +0100

    renamed another file for ordering purposes

--- Uebung/Uebung/src/prg09.java
+++ Uebung/Uebung/src/prg09.java
@@ -1,4 +1,4 @@
-public class prg9 {
+public class prg09 {
   public static void main(String[] args){
     System.out.println("Bitte gebe x ein! ");
     double x = new java.util.Scanner(System.in).nextDouble();
