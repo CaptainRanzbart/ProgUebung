a400be625eb911a77ff21365351c41f4ce81dc38
Colin Schmidt <colin@sei-com.de>
2022-11-30 14:53:10 +0100

    renamed two files for ordering purposes

--- Uebung/Uebung/src/prg08.java
+++ Uebung/Uebung/src/prg08.java
@@ -1,4 +1,4 @@
-public class prg8 {
+public class prg08 {
   public static void main(String[] args) {
     System.out.print("Wie viel Kapital in € wurde investiert? ");
     double invested = new java.util.Scanner(System.in).nextDouble();
