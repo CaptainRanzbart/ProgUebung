103d1da1f55d37be3556ac7c15b0d46a9f48b529
Colin Schmidt <colin@sei-com.de>
2023-03-15 13:39:25 +0100

    finished First Semester

---
+++ Uebung/Uebung/src/test.java
@@ -0,0 +1,10 @@
+public class test {
+  public static void main(String[] args) {
+    boolean[][] board = {
+        {false, false, false},
+        {false, false, false},
+        {true, true, true}
+    };
+  }
+  double input = new java.util.Scanner(System.in).nextDouble()
+}
